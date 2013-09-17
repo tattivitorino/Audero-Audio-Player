33f577a2e4fe123f66292e67be915dc497730cda
tattivitorino <tattivitorino@Tatianas-MacBook-Pro.local>
2013-09-17 15:45:38 -0300

    First Commit

---
+++ js/bookmark_bubble_msg_pt.js
@@ -0,0 +1,15 @@
+google.bookmarkbubble.Bubble.prototype.msg = {
+  android: 
+    '<b>Instale o EasyPP:</b><br /> 1) Adicione aos favoritos,<br /> 2) Toque e segure o bookmark,<br /> 3) Selecione "<b>Adicionar Atalho à Home</b>"',
+  android3: 
+    '<b>Instale o EasyPP:</b><br /> Toque em <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID3_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0 5px;" />,<br /> selecione "<b>Adicionar à</b>" e à "<b>Tela de Início</b>"',
+  android4: 
+    '<b>Instale o EasyPP:</b><br /> 1) Toque em <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_ANDROID4_MOBILE_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0 5px;" />,<br /> 2) Selecione "<b>Salvar nos Favoritos</b>",<br /> 3) Selecione "<b>Adicionar em</b>" e depois na "<b>Home</b>"',
+  blackberry: 
+    '<b>Instale o EasyPP:</b><br /> Toque em <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_BLACKBERRY_ICON_DATA_URL_ +'" style="height: 1em;display: inline-block;padding:0;margin:0 5px" />, selecione "<b>Adicionar à tela de início</b>"',
+  playbook: 
+     '<b>Instale o EasyPP:</b><br /> Toque em <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_PLAYBOOK_BOOKMARK_DATA_URL_ +'" style="height: 1.5em;display: inline-block;padding:0;margin:0 5px;" />, selecione  <br />"<b>Adicionar à tela de início</b>"',
+  ios42orlater :
+     '<b>Instale o EasyPP</b>:<br /> Toque em <img src="'+ google.bookmarkbubble.Bubble.prototype.IMAGE_SAFARI_FORWARD_DATA_URL_ +'" style="height: 1em;display: inline-block;padding: 0;margin: 0 5px" /> e depois em <b>"Adicionar à tela de início"</b>',
+  ioslegacy: '<b>Instale o EasyPP</b>:<br /> Toque em <b style="font-size:15px">+</b> e em <b>"Adicionar à tela de início"</b>'
+};
